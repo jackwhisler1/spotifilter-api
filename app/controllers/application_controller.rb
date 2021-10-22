d93c20782f630369ec10f97363d8c6dfcfc95059
jack <whisler1@gmail.com>
2021-10-22 11:53:25 -0500

    adds current user and authenticate user methods in app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,33 @@
 class ApplicationController < ActionController::API
+  def get_token
+    Rails.cache.fetch("access_token", expires_in: 1.hour) do
+
+    # Post request in do loop in case it doesn't find access token
+    end
+  end
+
+  # current user
+  def current_user
+    auth_headers = request.headers["Authorization"]
+    if auth_headers.present? && auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      token = auth_headers[/(?<=\A(Bearer ))\S+\z/]
+      begin
+        decoded_token = JWT.decode(
+          token,
+          Rails.application.credentials.fetch(:secret_key_base),
+          true,
+          { algorithm: "HS256" }
+        )
+        User.find_by(id: decoded_token[0]["user_id"])
+      rescue JWT::ExpiredSignature
+        nil
+      end
+    end
+  end
+
+  def authenticate_user
+    unless current_user
+      render json: {}, status: :unauthorized
+    end
+  end
 end
